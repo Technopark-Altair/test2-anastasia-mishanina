db97334f59a737607a6e70a023f7156a8552dc49
Anastasia Mishanina <mishanina.nastya@bk.ru>
2020-10-26 22:18:11 +0300

    Honework2
    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+firstOperand: number;
+ secondOperand: number;
+ operator:string;
+ result: number;
+ errorMessage: string;
+  doCalc(){
+    if (this.firstOperand !=null && this.secondOperand !=null && this.operator !=null) {
+       if (this.operator == "-"){
+        this.result = this.firstOperand - this.secondOperand;
+        this.errorMessage = "Нет ошибки";
+       }else if(this.operator =="+"){
+      this.result = this.firstOperand + this.secondOperand;
+        this.errorMessage = "Нет ошибки";
+    }else if(this.operator =="/"){
+if (this.secondOperand == 0) {
+  this.errorMessage=" На ноль делить нельзя";
+  this.result = null;
+}else{
+  this.result= this.firstOperand / this.secondOperand;
+  this.errorMessage = "Нет ошибки";
+
+}
+  }else if(this.operator =="*"){
+this.result = this.firstOperand * this.secondOperand;
+    this.errorMessage = "Нет ошибки";
+  }
+}else{
+  if(this.errorMessage == null){
+    this.errorMessage == "Нет первого операнда";
+  } else if ( this.secondOperand == null) {
+    this.errorMessage == "Нет второго операнда";
+  } else if (this.operator == null){
+    this.errorMessage =="Нет оператора действия";
+  }
+}
+  }
+
+
+
+
+
+
 }
