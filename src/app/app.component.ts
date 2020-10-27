a205d0a549695dad980a3ed95575c52d9fda14a1
Anastasia Mishanina <mishanina.nastya@bk.ru>
2020-10-27 15:11:16 +0300

    test 2
    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,26 +14,18 @@ firstOperand: number;
  errorMessage: string;
   doCalc(){
     if (this.firstOperand !=null && this.secondOperand !=null && this.operator !=null) {
-       if (this.operator == "-"){
-        this.result = this.firstOperand - this.secondOperand;
+       if (this.operator == "!"){
+       
+        this.errorMessage = "Отрицание от всего выражения";
+       }else if(this.operator =="&&"){
+      this.result = this.firstOperand && this.secondOperand;
         this.errorMessage = "Нет ошибки";
-       }else if(this.operator =="+"){
-      this.result = this.firstOperand + this.secondOperand;
-        this.errorMessage = "Нет ошибки";
-    }else if(this.operator =="/"){
-if (this.secondOperand == 0) {
-  this.errorMessage=" На ноль делить нельзя";
-  this.result = null;
-}else{
-  this.result= this.firstOperand / this.secondOperand;
+    }else if(this.operator =="||"){
+  this.result= this.firstOperand || this.secondOperand;
   this.errorMessage = "Нет ошибки";
 
 }
-  }else if(this.operator =="*"){
-this.result = this.firstOperand * this.secondOperand;
-    this.errorMessage = "Нет ошибки";
-  }
-}else{
+  
   if(this.errorMessage == null){
     this.errorMessage == "Нет первого операнда";
   } else if ( this.secondOperand == null) {
